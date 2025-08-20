30191fb4a6b4500db99bffc0b0b2335b9a02a48b
breno-xp <brexp14@gmail.com>
2025-08-20 18:09:47 -0300

    Implementação parcial de componente news

---
+++ src/components/News.jsx
@@ -0,0 +1,37 @@
+import './News.css';
+import { useState } from 'react';
+
+function News(){
+    const [hasMaxHeight, setHasMaxHeight] = useState(true);
+    
+    const toggleMaxHeight = () => {
+        setHasMaxHeight(!hasMaxHeight);
+    };
+
+    return(
+        <div className = "container">
+            <div id="head">
+                <h1>News</h1>
+                <button onClick={toggleMaxHeight}> {hasMaxHeight ? 'Show full articles' : 'Reduce articles'} </button>
+            </div>
+            <div className="news">
+                <div className='card'>
+                    <h2>Content</h2>
+                    <span className = {hasMaxHeight ? '' : 'off_maxHeight'}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint debitis sed architecto quisquam vel at laborum sit ducimus? Modi eligendi cupiditate maxime distinctio amet aut recusandae perferendis veritatis repellendus assumenda? Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quaerat dolor doloremque aspernatur, nesciunt animi quae similique consequuntur quisquam? Debitis ea quas eaque similique illo autem? Tempora, vitae. Ut, dolores?</span>
+                </div>
+                <div className='card'>
+                    <h2>Content</h2>
+                    <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint debitis sed architecto quisquam vel at laborum sit ducimus? Modi eligendi cupiditate maxime distinctio amet aut recusandae perferendis veritatis repellendus assumenda?</span>
+                </div>
+                <div className='card'>
+                    <h2>Content</h2>
+                    <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint debitis sed architecto quisquam vel at laborum sit ducimus? Modi eligendi cupiditate maxime distinctio amet aut recusandae perferendis veritatis repellendus assumenda?</span>
+                </div>
+            </div>
+        </div>
+    );
+
+    // {hasMaxHeight ? 'v' : '^'}
+}
+
+export default News
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx'
 
+
+import News from '../components/News.jsx'
+
 export function Home(){
   return (
     <>
       <Header/>
+
+
+      <News/>
       <Footer/>
     </>
   );
