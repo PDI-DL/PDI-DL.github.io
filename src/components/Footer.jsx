a7e45030237ddcc958cc727d2f00651d487877d8
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-08-20 21:00:32 -0300

    typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
   return(
     <footer>
       <div className='container'>
-        <span>&copy; Digital Image Processign and Machine Learning | {new Date().getFullYear()}</span>
+        <span>&copy; Digital Image Processing and Machine Learning | {new Date().getFullYear()}</span>
         <br />
         <br />
         <span><b>Address</b> C3 - Computer Science Center rooms: 218 and 214 | Brazil - Rio Grande - RS, 96203-900</span>
