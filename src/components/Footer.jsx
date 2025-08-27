15e17df86c194855f0b3e198c96123ff25bf4a6d
breno-xp <brexp14@gmail.com>
2025-08-27 16:52:19 -0300

    Finalização de protótipo de header e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,23 +10,18 @@ import ufsmLogo from "../assets/logos/logo_ufsm.png"
 function Footer(){
   return(
     <footer>
-      <div className="container">
-        <span>&copy; Digital Image Processing and Machine Learning | {new Date().getFullYear()}</span>
-        <br />
-        <br />
-        <span><b>Address</b> C3 - Computer Science Center rooms: 218 and 214 | Brazil - Rio Grande - RS, 96203-900</span>
+      <div className="container wrap">
+        <div className="footer_info">
+          <span>&copy; Digital Image Processing and Machine Learning | {new Date().getFullYear()}</span>
+          <br/>
+          <br/>
+          <span><b>Address</b> C3 - Computer Science Center rooms: 218 and 214 | Brazil - Rio Grande - RS, 96203-900</span>
+        </div>
         <div className="footerLogos">
-          <div className="headInstitutions">
             <h1>Institutions</h1>
             <Link to="#"><img src={c3Logo} alt="Computation Sciences Center logo" /></Link>
-            <Link to="#"><img src={nautecLogo} alt="Intelligent automation and robotics group" /></Link>
+            <Link to="#"><img id = "nautec" src={nautecLogo} alt="Intelligent automation and robotics group" /></Link>
             <Link to="#"><img src={furgLogo} alt="Federal University of Rio Grande logo" /></Link>
-          </div>
-          <div className="otherInstitutions">
-            <h1>Partners</h1>
-            <Link to="#"><img src={ufamLogo} alt="" /></Link>
-            <Link to="#"><img src={ufsmLogo} alt="" /></Link>
-          </div>
         </div>
       </div>
     </footer>
