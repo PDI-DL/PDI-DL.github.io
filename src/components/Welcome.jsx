34e923e033ca6b37e20641745173f54e1ca53521
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-08-29 14:01:04 -0300

    adicionado marquee

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+export function Welcome() {
+    return (
+        <>
+            <h1>Welcome</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem labore illum blanditiis necessitatibus recusandae consequuntur est dolorum facilis nobis maiores dolor velit voluptates placeat, dolore saepe aliquid inventore quae ipsum! Asperiores officiis iure id corporis sapiente, sint reiciendis omnis similique, magni rem doloremque incidunt. Necessitatibus expedita eligendi nemo ex, cupiditate odio accusantium inventore temporibus. Dolorum labore eos nisi quidem est! Dignissimos provident eligendi, fuga porro maiores molestias. Iure tempore, vero praesentium expedita consequuntur ipsam adipisci quisquam sint explicabo voluptatem impedit repudiandae modi eveniet veritatis quaerat aspernatur excepturi voluptate recusandae dolorum!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem labore illum blanditiis necessitatibus recusandae consequuntur est dolorum facilis nobis maiores dolor velit voluptates placeat, dolore saepe aliquid inventore quae ipsum! Asperiores officiis iure id corporis sapiente, sint reiciendis omnis similique, magni rem doloremque incidunt. Necessitatibus expedita eligendi nemo ex, cupiditate odio accusantium inventore temporibus. Dolorum labore eos nisi quidem est! Dignissimos provident eligendi, fuga porro maiores molestias. Iure tempore, vero praesentium expedita consequuntur ipsam adipisci quisquam sint explicabo voluptatem impedit repudiandae modi eveniet veritatis quaerat aspernatur excepturi voluptate recusandae dolorum!</p>
+        </>
+    )
+}
+
+export default Welcome;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,9 @@ import { useEffect } from "react";
 
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import Welcome from "../components/Welcome.jsx";
 import Carousel from "../components/Carousel.jsx";
-import News from "../components/News.jsx";
+import MarqueeLogos from "../components/MarqueeLogos.jsx";
 
 export function Home(){
     useEffect(() => {
@@ -14,9 +15,8 @@ export function Home(){
             <Header/>
             <Carousel/>
             <main>
-                <h1>Welcome</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem labore illum blanditiis necessitatibus recusandae consequuntur est dolorum facilis nobis maiores dolor velit voluptates placeat, dolore saepe aliquid inventore quae ipsum! Asperiores officiis iure id corporis sapiente, sint reiciendis omnis similique, magni rem doloremque incidunt. Necessitatibus expedita eligendi nemo ex, cupiditate odio accusantium inventore temporibus. Dolorum labore eos nisi quidem est! Dignissimos provident eligendi, fuga porro maiores molestias. Iure tempore, vero praesentium expedita consequuntur ipsam adipisci quisquam sint explicabo voluptatem impedit repudiandae modi eveniet veritatis quaerat aspernatur excepturi voluptate recusandae dolorum!</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem labore illum blanditiis necessitatibus recusandae consequuntur est dolorum facilis nobis maiores dolor velit voluptates placeat, dolore saepe aliquid inventore quae ipsum! Asperiores officiis iure id corporis sapiente, sint reiciendis omnis similique, magni rem doloremque incidunt. Necessitatibus expedita eligendi nemo ex, cupiditate odio accusantium inventore temporibus. Dolorum labore eos nisi quidem est! Dignissimos provident eligendi, fuga porro maiores molestias. Iure tempore, vero praesentium expedita consequuntur ipsam adipisci quisquam sint explicabo voluptatem impedit repudiandae modi eveniet veritatis quaerat aspernatur excepturi voluptate recusandae dolorum!</p>
+                <Welcome />
+                <MarqueeLogos />
             </main>
             <Footer/>
         </>
