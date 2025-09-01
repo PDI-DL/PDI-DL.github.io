4c81d00a83f06979659a751452f39f667aa2a75e
breno-xp <brexp14@gmail.com>
2025-09-01 16:03:40 -0300

    Resolução de conflitos 2 e final

--- src/components/MarqueeLogos.jsx
+++ src/components/MarqueeLogos.jsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee";
 import c3Logo from "../assets/logos/logo_c3.png";
 import nautecLogo from "../assets/logos/logo_nautec-dark.png";
 import furgLogo from "../assets/logos/logo_furg.png";
-import ufamLogo from "../assets/logos/logo_ufam.png"
-import ufsmLogo from "../assets/logos/logo_ufsm.png"
+import ufamLogo from "../assets/logos/logo_ufam.png";
+import ufsmLogo from "../assets/logos/logo_ufsm.png";
 
 import "../style/components/MarqueeLogos.css";
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,7 @@ import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import Welcome from "../components/Welcome.jsx";
 import Carousel from "../components/Carousel.jsx";
-<<<<<<< HEAD
-=======
 import MarqueeLogos from "../components/MarqueeLogos.jsx";
->>>>>>> ffe9c5000ff81157385bb46235d21c013f90fad9
 
 export function Home(){
     useEffect(() => {
