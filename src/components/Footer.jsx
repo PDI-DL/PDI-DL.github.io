b4db5bef5b1d8442569f41ccf1ed7464024a48a3
breno-xp <brexp14@gmail.com>
2025-09-01 17:57:20 -0300

    Pequenos ajustes no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,15 +15,11 @@ function Footer(){
                 <div className="footerInfo">
                     <span>Digital Image Processing and Machine Learning <b>Research Group</b> | {new Date().getFullYear()}</span>
                     <br/>
-                    <br/>
-                    {/* <span><b>Address</b> C3 - Computer Science Center rooms: 218 and 214 | Brazil - Rio Grande - RS, 96203-900</span> */}
                     <span> Computer Science Center (<b>C3</b>) - Federal University of Rio Grande (<b>FURG</b>)</span>
-                    <br />
                     <br />
                     <span><b>Address</b> av. Itália street, 96203-900 | Rio Grande - RS - Brazil</span>
                 </div>
                 <div className="footerLogos">
-                    <h1>-Institutions-</h1>
                     <div className="Logos">
                         <Link to="#"><img src={c3Logo} alt="Computation Sciences Center logo" /></Link>
                         <Link to="#"><img src={nautecLogo} id="nautec" alt="Intelligent automation and robotics group" /></Link>
