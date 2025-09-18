76a0ce4ec1ac8abdd69ad4c1d17d97d0b8269092
breno-xp <brexp14@gmail.com>
2025-09-18 17:58:16 -0300

    Pequenas modificações em publications e em team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,7 +11,7 @@ export function Team_component(){
         <>
         <div>
             <div className="head">
-                <h1>Team</h1>
+                <h1>Research team</h1>
             </div>
             <h2 className="teamSec"> Faculty </h2>
             <section>
@@ -93,7 +93,8 @@ export function Team_component(){
                         </div>
                     </div>
                 </div>
-            </section><h2 className="teamSec">MSc students</h2>
+            </section>
+            <h2 className="teamSec">MSc students</h2>
             <section>
                 <div className="profile">
                     <img src={unknownPic} alt="Unset profile picture" />
@@ -143,7 +144,8 @@ export function Team_component(){
                         </div>
                     </div>
                 </div>        
-            </section><h2 className="teamSec">Undergraduate students</h2>
+            </section>
+            <h2 className="teamSec">Undergraduate students</h2>
             <section>
                 <div className="profile">
                     <img src={unknownPic} alt="Unset profile picture" />
@@ -228,7 +230,72 @@ export function Team_component(){
                             <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
                         </div>
                     </div>
-                </div>                
+                </div>
+            </section>
+            <h2 className="teamSec">Research associates and visitors</h2>                
+            <section>
+                <div className="profile">
+                    <img src={unknownPic} alt="Unset profile picture" />
+                    <div className="info">
+                        <h2>Pessoa Fulano dos Santos</h2>
+                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <div className="infoLinks">
+                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
+                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="profile">
+                    <img src={unknownPic} alt="Unset profile picture" />
+                    <div className="info">
+                        <h2>Pessoa Fulano dos Santos</h2>
+                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <div className="infoLinks">
+                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
+                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="profile">
+                    <img src={unknownPic} alt="Unset profile picture" />
+                    <div className="info">
+                        <h2>Pessoa Fulano dos Santos</h2>
+                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <div className="infoLinks">
+                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
+                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <h2 className="teamSec">Alumni</h2>
+            <section>
+                <div className="profile">
+                    <img src={unknownPic} alt="Unset profile picture" />
+                    <div className="info">
+                        <h2>Pessoa Fulano dos Santos</h2>
+                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <div className="infoLinks">
+                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
+                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                        </div>
+                    </div>
+                </div><div className="profile">
+                    <img src={unknownPic} alt="Unset profile picture" />
+                    <div className="info">
+                        <h2>Pessoa Fulano dos Santos</h2>
+                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <div className="infoLinks">
+                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
+                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
         </>
