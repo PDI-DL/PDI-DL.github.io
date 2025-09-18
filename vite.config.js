216152de8349e45d800054458e6cc725421146af
breno-xp <brexp14@gmail.com>
2025-09-18 16:27:26 -0300

    Mudança em base de vite.config.js para novo nome de repositório

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/Site_PDI", // As in the repository's name
+    base: "/PDI_web", // As in the repository's name
 })
