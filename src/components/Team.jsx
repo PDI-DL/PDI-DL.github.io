aba261db2ceefe15754e994a3d93caaac46a70cd
breno-xp <brexp14@gmail.com>
2025-09-29 18:09:32 -0300

    Pequenas alterações em team e de estilo geralt

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,6 +2,8 @@ import unknownPic from "../assets/other/profilePicture_placeholder.png"
 import websiteIcon from "../assets/team/icons/globe-grid.png"
 import linkedinIcon from "../assets/logos/logo_linkedin-dark.png"
 import academicIcon from "../assets/team/icons/graduation-hat.png"
+import githubIcon from "../assets/logos/logo_github-light.png"
+import emailIcon from "../assets/team/icons/email.png"
 
 import "../style/components/Team.css"
 
@@ -11,7 +13,7 @@ export function Team_component(){
         <>
         <div>
             <div className="head">
-                <h1>Research team</h1>
+                <h1>Team</h1>
             </div>
             <h2 className="teamSec"> Faculty </h2>
             <section>
@@ -150,24 +152,25 @@ export function Team_component(){
                 <div className="profile">
                     <img src={unknownPic} alt="Unset profile picture" />
                     <div className="info">
-                        <h2>Pessoa Fulano dos Santos</h2>
-                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <h2>Emanuel da Costa Silva</h2>
+                        <p>Computer Vision, Deep Learning, Underwater Imagery, Autonomous Navigation, Computational Imaging, Digital Image Processing</p>
                         <div className="infoLinks">
-                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
-                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
-                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                            <a href="https://emanuelcostas.github.io/" target="blank"><img src={websiteIcon} alt="Personal website icon" /></a>
+                            <a href="https://github.com/EmanuelCostaS" target="blank"><img src={githubIcon} alt="Github icon" /></a>
+                            <a href="https://www.linkedin.com/in/emanuel-da-costa/" target="blank"><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href="mailto:silva2004@hotmail.com"><img src={emailIcon} alt="Email icon" /></a>
                         </div>
                     </div>
                 </div>
                 <div className="profile">
                     <img src={unknownPic} alt="Unset profile picture" />
                     <div className="info">
-                        <h2>Pessoa Fulano dos Santos</h2>
-                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
+                        <h2>Guilherme Louro Mano Costa</h2>
+                        <p>Computer Vision, Digital Image Processing, Deep Learning,  ROS, Autonomous industrial robotics, Autonomous Navigation</p>
                         <div className="infoLinks">
-                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
-                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
-                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
+                            <a href="https://github.com/GuilhermeMano" target="blank"><img src={githubIcon} alt="Github icon" /></a>
+                            <a href="https://www.linkedin.com/in/guilherme-mano-994351297"><img src={linkedinIcon} alt="LinkedIn icon" /></a>
+                            <a href="mailto:guilhermemano65667@gmail.com"><img src={emailIcon} alt="Email icon" /></a>
                         </div>
                     </div>
                 </div>
@@ -259,32 +262,6 @@ export function Team_component(){
                     </div>
                 </div>
                 <div className="profile">
-                    <img src={unknownPic} alt="Unset profile picture" />
-                    <div className="info">
-                        <h2>Pessoa Fulano dos Santos</h2>
-                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
-                        <div className="infoLinks">
-                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
-                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
-                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <h2 className="teamSec">Alumni</h2>
-            <section>
-                <div className="profile">
-                    <img src={unknownPic} alt="Unset profile picture" />
-                    <div className="info">
-                        <h2>Pessoa Fulano dos Santos</h2>
-                        <p>Pequeno texto de apresentação profissional aqui, áreas que trabalha, etc...</p>
-                        <div className="infoLinks">
-                            <a href="https://breno-xp.github.io/Site_PDI/"><img src={websiteIcon} alt="Personal website icon" /></a>
-                            <a href=""><img src={linkedinIcon} alt="LinkedIn icon" /></a>
-                            <a href=""><img src={academicIcon} alt="Scholar icon" /></a>
-                        </div>
-                    </div>
-                </div><div className="profile">
                     <img src={unknownPic} alt="Unset profile picture" />
                     <div className="info">
                         <h2>Pessoa Fulano dos Santos</h2>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,8 +4,7 @@ export function Welcome() {
     return (
         <div className="welcome-container">
             <h1>Welcome</h1>
-            <p>Welcome to the website of the Deep Learning and Digital Image Processing (PDI + DL) Laboratory at the Federal University of Rio Grande (FURG). The group, led by Professor Paulo Drews Jr., comprises students with diverse academic backgrounds in Automation Engineering, Computer Engineering, Information Systems and Mathematics. Our primary research focuses on the developmebnt of Machine Learning methods for detection, application of sensors and radars, with a principal emphasis on underwater environments.</p>
-            {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem labore illum blanditiis necessitatibus recusandae consequuntur est dolorum facilis nobis maiores dolor velit voluptates placeat, dolore saepe aliquid inventore quae ipsum! Asperiores officiis iure id corporis sapiente, sint reiciendis omnis similique, magni rem doloremque incidunt. Necessitatibus expedita eligendi nemo ex, cupiditate odio accusantium inventore temporibus. Dolorum labore eos nisi quidem est! Dignissimos provident eligendi, fuga porro maiores molestias. Iure tempore, vero praesentium expedita consequuntur ipsam adipisci quisquam sint explicabo voluptatem impedit repudiandae modi eveniet veritatis quaerat aspernatur excepturi voluptate recusandae dolorum!</p> */}
+            <p>Welcome to the website of the Deep Learning and Digital Image Processing (PDI + DL) group at the Federal University of Rio Grande (FURG). The team, led by Professor Paulo Drews Jr., comprises students with diverse academic backgrounds in Automation Engineering, Computer Engineering, Information Systems and Mathematics. Our primary research focuses on the developmebnt of Machine Learning methods for detection, application of sensors and radars, with a principal emphasis on underwater environments.</p>
         </div>
     )
 }
