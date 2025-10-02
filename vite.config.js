94f0edd93f1a99ac485beecf6cb81bd8aa1b7ad5
breno-xp <brexp14@gmail.com>
2025-10-02 14:44:28 -0300

    Mudanças em package.json e vite.config.js para novo domínio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/PDI_web", // As in the repository's name
+    base: "/PDI-DL", // As in the repository's name
 })
