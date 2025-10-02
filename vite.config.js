056d472b28fca815e8736edc87ccedc74ac0494b
breno-xp <brexp14@gmail.com>
2025-10-02 14:52:14 -0300

    Continuação de mudanças para novo domínio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/PDI-DL", // As in the repository's name
+    base: "/PDI-DL.github.io", // As in the repository's name
 })
